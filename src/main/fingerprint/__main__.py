92f6a61cb00303b1b99a98fe65aaa6edbffbb4e5
nedrazzz <smg9972@gmail.com>
2025-04-27 12:56:02 +0900

    refactor: main.fingerprint 코드 가독성 향상

--- src/main/fingerprint/__main__.py
+++ src/main/fingerprint/__main__.py
@@ -1,226 +1,178 @@
 """
 YouTube 오디오 추출 및 지문 생성 배치 처리 애플리케이션 메인 모듈
 """
+from dataclasses import dataclass
 import re
+import shutil
 import traceback
 import argparse
 import gc
 from pathlib import Path
 import logging
-import os
+from typing import Any, List, Tuple
 import essentia.standard as es
 
 from src.fingerprint_manager.fingerprint_generator import FingerprintGenerator
 from src.utils.file_manager import FingerprintDB
 from src.utils.memory_manager import monitor_memory
 from src.youtube_downloader.audio_batch_downloader import download_youtube_audio_batch
-# from src.youtube_downloader.audio_downloader import download_youtube_audio
 
 # 로깅 설정
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    format='%(levelname)s - %(message)s'
 )
 logger = logging.getLogger(__name__)
 
-def read_youtube_urls(file_path: str) -> list:
-    """
-    텍스트 파일에서 YouTube URL 목록을 읽어옵니다.
+def read_youtube_urls(file_path: Path) -> list:
+    logger.info(f"{file_path} 경로에서 YouTube URL 리스트를 가져오는 중...")
+
+    # 파일 여부 검증
+    if not file_path.exists():
+        raise FileNotFoundError(f"{file_path} 경로에서 파일을 못찾았습니다.")
     
-    Args:
-        file_path (str): YouTube URL이 포함된 텍스트 파일 경로
-        
-    Returns:
-        list: YouTube URL 리스트
-        
-    Raises:
-        FileNotFoundError: 파일을 찾을 수 없는 경우
-        IOError: 파일을 읽을 수 없는 경우
-    """
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"URL 파일을 찾을 수 없습니다: {file_path}")
-    
+    # 파일에서 유튜브 url 읽기
+    urls = []
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
             # 각 줄을 읽고, 빈 줄과 공백을 제거
-            urls = [line.strip() for line in file if line.strip()]
-        
-        if not urls:
-            logger.warning("URL 파일이 비어 있습니다.")
-        else:
-            logger.info(f"{len(urls)}개의 YouTube URL을 로드했습니다.")
-        
-        return urls
+            for line in file:
+                 line = line.strip()
+                 if line:
+                     urls.append(line)
+                     logger.info(f"\tRead URL: {line}")
     except IOError as e:
-        logger.error(f"URL 파일 읽기 오류: {e}")
+        logger.error(f"URL 파일을 읽을 때 오류가 발생했습니다: {e}")
         raise
-
-def process_youtube_urls(
-    urls: list,
-    download_dir: Path = None
-):
-    """
-    여러 YouTube URL에서 오디오를 다운로드합니다.
-    
-    Args:
-        urls (list): YouTube URL 리스트
-        start_time (str): 시작 시간 (HH:MM:SS 형식)
-        end_time (str): 종료 시간 (HH:MM:SS 형식)
-        audio_format (str, optional): 오디오 형식. 기본값 "wav".
-        audio_quality (str, optional): 오디오 품질. 기본값 "192".
-        download_dir (Path, optional): 다운로드 디렉토리. 기본값 None.
         
-    Returns:
-        tuple: (성공적으로 다운로드된 파일의 경로 리스트, 실패한 URL 리스트)
-    """
-    # 메모리 모니터링
-    monitor_memory()
-    
-    # YouTube에서 다운로드
-    downloaded_files = download_youtube_audio_batch(youtube_urls=urls, 
-                                                    download_dir=download_dir,
-                                                    end_time="00:00:30")
-    if not downloaded_files:
-        raise Exception(f"빈 파일 정보: {downloaded_files}")
-    print(f"✓ 다운로드 완료")
+    # 로그 출력
+    if not urls:
+        raise Exception("URL을 하나 아상 가져오지 못했습니다.")
 
-    return downloaded_files
+    logger.info(f"읽은 YouTube URL 리스트: {len(urls)}개")
+    
+    return urls
+
+def download_youtube_audio(
+    urls: list,
+) -> Path:
+    # YouTube URL에서 오디오 다운로드
+    downloads_count, download_dir = download_youtube_audio_batch(youtube_urls=urls, 
+                                                start_time="00:00:00",
+                                                end_time="00:00:30")
+    if downloads_count == 0:
+        raise Exception("오디오를 하나 이상 다운로드 받지 못했습니다.")
+    return download_dir
         
 
 def generate_fingerprints(
-    download_results: list,
-    audio_dir: str,
+    audio_dir: Path,
+)-> List[Tuple[str, Any]]:
+    logger.info(f"{audio_dir} 폴더에서 오디오를 오디오 지문으로 변환 중...")
+
+    fingerprint_generator = FingerprintGenerator()
+
+    processed_count = 0 # 지문 변환 성공 횟수
+    failed_count = 0 # 지문 변환 실패 횟수
+    fingerprints = [] # 오디오 지문 저장 변수
+    
+    # 디렉토리에서 오디오 파일들을 읽어서 오디오 지문 변환
+    for audio_path in audio_dir.glob("*.wav"):
+        #오디오의 노래 제목 가져오기
+        audio_name = re.sub(r'(.*?)\s+\[[^\]]*\]$', r'\1', Path(audio_path).stem)
+
+        try:
+            # 오디오 파일 로드 및 지문 생성
+            sample_rate = es.MetadataReader(filename=str(audio_path))()[-2]
+            audio_file = es.MonoLoader(filename=str(audio_path), sampleRate=sample_rate)()
+
+            # 오디오 지문 생성
+            fingerprint = fingerprint_generator.get_spectrogram_fingerprint(audio_file, sample_rate)
+        except Exception as e:
+            # 지문 생성 실패 시
+            failed_count += 1
+            logger.error(f"지문 생성 실패: {audio_name}")
+            # logger.error(f"{e}")
+            traceback.print_exc()
+            continue
+            
+        # 지문 생성 성공 시
+        processed_count += 1
+        fingerprints.append((audio_name, fingerprint))
+
+    if processed_count == 0:
+        raise Exception("아무 지문도 생성하지 못했습니다.")
+
+    logger.info(f"지문 생성 완료: 성공 {processed_count}개 실패 {failed_count}개")
+
+    return fingerprints
+
+def save_fingerprints(
+    fingerprints: List[Tuple[str, Any]],
     worldcup_name: str,
 ) -> tuple:
     """
     다운로드된 오디오 파일에서 지문을 생성하고 데이터베이스에 저장합니다.
-    
-    Args:
-        download_results (list): (오디오 파일 경로, 파일 이름) 튜플의 리스트
-        worldcup_id (int): 연결할 월드컵 ID
-        fingerprint_generator (FingerprintGenerator): 지문 생성기 인스턴스
-        db_manager (DatabaseManager): 데이터베이스 관리자 인스턴스
-        
-    Returns:
-        tuple: (성공적으로 처리된 파일 수, 실패한 파일 수)
     """
+    logger.info(f"오디오 지문 데이터베이스에 저장 중...")
+
     db_manager = FingerprintDB()
-    fingerprint_generator = FingerprintGenerator()
+    for name, fingerprint in fingerprints:
+        db_manager.insert_changpop(name, fingerprint, worldcup_name)
+        logger.info(f"지문 저장 완료: {name}")
 
-    processed_count = 0
-    failed_count = 0
-    
-    for download_path in download_results:
-        try:
-            #오디오 지문의 이름 가져오기
-            audio_name = Path(download_path).stem
-            filtered_audio_name = re.sub(r'(.*?)\s+\[[^\]]*\]$', r'\1', audio_name)
-            audio_path = audio_dir + '/' + audio_name + '.wav'
-
-            print(f"지문 생성 중: {audio_path}...")
-            # 오디오 파일 로드 및 지문 생성
-            sample_rate = es.MetadataReader(filename=audio_path)()[-2]
-            audio_file = es.MonoLoader(filename=audio_path, sampleRate=sample_rate)()
-            fingerprint = fingerprint_generator.get_spectrogram_fingerprint(audio_file, sample_rate)
-            
-            # 데이터베이스에 저장
-            print(f"데이터베이스에 저장 중: {audio_path}...")
-            db_manager.insert_changpop(filtered_audio_name, fingerprint, worldcup_name)
-            
-            processed_count += 1
-            print(f"지문 생성 및 저장 완료: {audio_path}")
-            
-        except Exception as e:
-            failed_count += 1
-            print(f"지문 생성 실패: {audio_path}, 오류: {str(e)}")
-            logger.error(f"지문 생성 실패: {audio_path}, 오류: {str(e)}")
-            traceback.print_exc()
-            continue
-        finally:
-            # 메모리 정리
-            gc.collect()
-    
-    return processed_count, failed_count
+# 메임 함수 인자
+@dataclass
+class TypedArgs:
+    url_file: Path
+    worldcup_name: str
 
 def get_parameters():
-    parser = argparse.ArgumentParser(description="YouTube 오디오 배치 다운로드 및 지문 생성 도구")
+    parser = argparse.ArgumentParser(description="YouTube URL에서 오디오 다운로드 및 지문 생성 도구")
     
-    # 배치 다운로드 관련 인자
-    parser.add_argument("--file", type=str, required=True, 
+    parser.add_argument("-u", "--urls", type=str, required=True, 
                         help="YouTube URL이 포함된 텍스트 파일 경로")
-    parser.add_argument("--dir", type=str, default="download", 
-                        help="다운로드 디렉토리 경로")
-    # 지문 생성 관련 인자
-    parser.add_argument("--fingerprint", action="store_true", 
-                        help="다운로드 후 지문 생성 및 저장 활성화")
     parser.add_argument("-n", "--name", 
                         help="지문 컬렉션 이름 (지문 생성 시 필수)")
     args = parser.parse_args()
-    
-    # 지문 생성이 활성화된 경우 필수 인자 확인
-    if args.fingerprint and not args.name:
-        parser.error("지문 생성을 위해서는 --name 및 --genre 인자가 필요합니다.")
 
-    return args
+    # 모듈 실행 파라미터 출력
+    logger.info(f"URL 파일: {args.urls}")
+    logger.info(f"월드컵 지문 이름: {args.name}")
+
+    return TypedArgs(Path(args.urls), args.name)
 
 def main():
     """메인 실행 함수"""
+    # 메인 함수 인자 가져오기
+    print()
     args = get_parameters()
     
-    # 메인 헤더 출력``
-    print("=======================================")
-    print(f"URL 파일: {args.file}")
-    print(f"다운로드 디렉토리: {args.dir if args.dir else '기본값'}")
-    if args.fingerprint:
-        print("지문 생성: 활성화")
-        print(f"지문 월드컵 이름: {args.name}")
-    print("=======================================")
+    # 유튜브 url 리스트 읽기
+    print()
+    youtube_urls = read_youtube_urls(args.url_file)
     
+    # 유튜브 오디오 배치 다운로드 수행
+    print()
+    audio_dir = download_youtube_audio(youtube_urls)
+
+    # 오디오 지문 생성
     try:
-        monitor_memory()
+        print()
+        fingerprints = generate_fingerprints(audio_dir)
 
-        # URL 리스트 읽기
-        youtube_urls = read_youtube_urls(args.file)
-        print(f"처리할 URL 수: {len(youtube_urls)}")
-        
-        # 다운로드 디렉토리 설정
-        download_dir = Path(args.dir)
-        download_dir.mkdir(exist_ok=True, parents=True)
-        
-        # 배치 다운로드 수행
-        downloaded_files = process_youtube_urls(
-            youtube_urls, 
-            download_dir
-        )
-        
-        # 다운로드 결과 출력
-        print("=======================================")
-        print(f"다운로드 성공: {len(downloaded_files)}개 파일")
-        
-        # 지문 생성 및 저장
-        if args.fingerprint and downloaded_files:
-            print("\n=======================================")
-            print("지문 생성 및 저장 시작...")
-            
-            # 지문 생성 및 저장
-            processed_count, failed_count = generate_fingerprints(
-                downloaded_files,
-                download_dir.stem,
-                args.name,
-            )
-            
-            # 지문 생성 결과 출력
-            print("\n=======================================")
-            print(f"지문 생성 성공: {processed_count}개 파일")
-            print(f"지문 생성 실패: {failed_count}개 파일")
-
-    except Exception as e:
-        print(f"배치 처리 실패: {e}")
-        traceback.print_exc()
+        # 오디오 지문 저장
+        print()
+        save_fingerprints(fingerprints, args.worldcup_name)
     finally:
-        # 강제 메모리 정리
-        gc.collect()
-        monitor_memory()
+        # 다운로드한 오디오 삭제
+        shutil.rmtree(audio_dir)
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        logger.error(f"오디오 지문 생성 실패: {e}")
+        traceback.print_exc()
+    finally:
+        gc.collect()
--- src/youtube_downloader/audio_batch_downloader.py
+++ src/youtube_downloader/audio_batch_downloader.py
@@ -1,56 +1,39 @@
+import re
+import tempfile
 import yt_dlp
 from pathlib import Path
-import re
 import logging
-from typing import Optional
+from typing import List, Tuple
 
 logger = logging.getLogger(__name__)
 
 def download_youtube_audio_batch(
-    youtube_urls: list[str], 
+    youtube_urls: List[str], 
     start_time: str = "00:00:00", 
     end_time: str = "00:10:00", 
     audio_format: str = "wav", 
     audio_quality: str = "192",
-    download_dir: Optional[Path] = None
-):
+) -> Tuple[int, Path]:
     """
     여러 YouTube 비디오에서 특정 시간 구간의 오디오를 일괄 다운로드합니다.
-
-    Args:
-        youtube_urls (list[str]): YouTube 비디오 URL 목록
-        start_time (str): 시작 시간 (HH:MM:SS 형식)
-        end_time (str): 종료 시간 (HH:MM:SS 형식)
-        audio_format (str, optional): 오디오 형식 (mp3, wav, m4a 등). 기본값 "wav".
-        audio_quality (str, optional): 오디오 품질 (kbps). 기본값 "192".
-        download_dir (Path, optional): 다운로드할 디렉토리 경로.
-                                      기본값은 현재 파일 기준 "../downloads".
-
-    Returns:
-        list[tuple[str, str]]: 다운로드된 오디오 파일 경로와 파일명의 튜플 목록
     """
-    # 다운로드 디렉토리 설정
-    if download_dir is None:
-        download_dir = Path(__file__).resolve().parent.parent.parent / "download"
-    
-    # 다운로드 디렉토리 생성
-    download_dir.mkdir(exist_ok=True, parents=True)
+    # 일시적인 다운로드 디렉토리 생성
+    download_dir = tempfile.mkdtemp()
 
-    downloaded_file_paths = [] # 다운로드된 파일 정보를 저장할 리스트
+    # 유튜브 오디오 성공 횟수 hook
+    success_downloads = 0
     def save_path_hook(data):
         # 다운로드 완료 상태일 때만 처리
-        if data['status'] == 'finished':
-            file_path = data['filename']
-            # 전체 경로 저장
-            downloaded_file_paths.append(file_path)
-            print(f'\n다운로드 완료: {file_path}')
+        if data["status"] == "finished":
+             nonlocal success_downloads
+             success_downloads += 1
     
     try:
         # 일괄 다운로드 설정
         logger.info(f"{start_time}부터 {end_time}까지 {len(youtube_urls)}개 오디오 다운로드 시작")
         ydl_opts = {
             "format": "bestaudio/best",
-            "paths": {"home": str(download_dir)},
+            "paths": {"home": download_dir},
             "postprocessors": [{
                 "key": "FFmpegExtractAudio",
                 "preferredcodec": audio_format,
@@ -67,5 +50,5 @@ def download_youtube_audio_batch(
     except Exception as e:
         logger.error(f"다운로드 중 오류 발생: {e}", exc_info=True)
     
-    logger.info(f"총 {len(downloaded_file_paths)}/{len(youtube_urls)}개 동영상 다운로드 완료")
-    return downloaded_file_paths
+    logger.info(f"총 {success_downloads}/{len(youtube_urls)}개 동영상 다운로드 완료")
+    return  success_downloads, Path(download_dir)
